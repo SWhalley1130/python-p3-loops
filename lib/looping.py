7a071da8c3b1de454f831ac9e7f441352947f3a3
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-14 14:39:09 -0600

    Completes lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list=[element*element for element in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(100):
+        if (i+1)%3==0 and (i+1)%5==0:
+            print("FizzBuzz")
+        elif (i+1)%3==0:
+            print("Fizz")
+        elif(i+1)%5==0:
+            print("Buzz")
+        else:
+            print(i+1)
+
+print(fizzbuzz())
